f2fbc93b61e6a44bdb21e1c2d9c68ef14171cfee
tony-andreev94 <tony.andreev94@outlook.com>
2020-01-14 14:43:50 +0200

    # # Write a program to read a list of integers, finds the smallest item and prints it.

--- 02. Lists/03. Smallest number.py
+++ 02. Lists/03. Smallest number.py
@@ -1,7 +1,7 @@
 # Write a program to read a list of integers, finds the smallest item and prints it.
 
 
-def find_smallest_number(input_list):
+def find_min_number(input_list):
     min_number = input_list[0]
     for x in input_list:
         if x < min_number:
@@ -12,4 +12,4 @@ def find_smallest_number(input_list):
 
 if __name__ == "__main__":
     my_list = [int(x) for x in input().split()]
-    print(find_smallest_number(my_list))
+    print(find_min_number(my_list))
