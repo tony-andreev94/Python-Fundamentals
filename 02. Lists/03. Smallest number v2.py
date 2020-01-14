36f81a68ff865a51ad04647c441b3fb8016bb52e
tony-andreev94 <tony.andreev94@outlook.com>
2020-01-14 14:48:16 +0200

    # Another solution for the same task.

---
+++ 02. Lists/03. Smallest number v2.py
@@ -0,0 +1,13 @@
+# Write a program to read a list of integers, finds the smallest item and prints it.
+# This is another solution, instead to search for the smallest number the list is sorted and the first item is printed
+
+
+def list_sort_func(input_list):
+    sorted_list = sorted(input_list)
+
+    return sorted_list[0]
+
+
+if __name__ == "__main__":
+    my_list = [int(x) for x in input().split()]
+    print(list_sort_func(my_list))
