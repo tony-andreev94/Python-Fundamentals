12559ee2ece603cd6a177749ae2b699f3203423c
tony-andreev94 <tony.andreev94@outlook.com>
2020-01-14 16:46:25 +0200

    # A program that reads a list of integers and finds how many of them are odd.

--- 02. Lists/05. Count Odd Numbers.py
+++ 02. Lists/05. Count Odd Numbers.py
@@ -1,25 +1,17 @@
 # Write a program to read a list of integers and find how many odd items it holds.
 
 
-def convert_to_int_list(input_list):
-    int_list = []
-    for x in input_list:
-        if x != '-':
-            int_list.append(int(x))
-
-    return int_list
-
-
 def count_odd_numbers(input_list):
     odd_counter = 0
     for x in input_list:
-        if x % 2 != 0:
+        # if x % 2 != 0:
+        if not x % 2 == 0:
             odd_counter += 1
 
     return odd_counter
 
 
 if __name__ == "__main__":
-    my_list = input().split(" ")
-    result = count_odd_numbers(convert_to_int_list(my_list))
+    my_list = [int(x) for x in input().split(" ")]
+    result = count_odd_numbers(my_list)
     print(result)
