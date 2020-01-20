f1b07ab37c08a3430c0ff81f5eb106e02f8156f0
tony-andreev94 <tony.andreev94@outlook.com>
2020-01-20 11:43:19 +0200

    # Basic task for character operations.

---
+++ 00. Basics - Data-Types, Variables/02. Chars to String.py
@@ -0,0 +1,9 @@
+# Write a function that receives 3 characters. Combine all the characters into one string and print it on the console
+
+char_one = input()
+char_two = input()
+char_three = input()
+
+print(char_one, end="")
+print(char_two, end="")
+print(char_three, end="")
