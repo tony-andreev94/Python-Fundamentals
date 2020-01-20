33c6cb27b5ed133b6d31aca031a49c3d22eda70e
tony-andreev94 <tony.andreev94@outlook.com>
2020-01-20 11:39:28 +0200

    # Basic task for number operations.

--- 00. Basics - Data-Types, Variables/01. Integer Operations.py
+++ 00. Basics - Data-Types, Variables/01. Integer Operations.py
@@ -1,1 +1,17 @@
+# Read four integer numbers. Add first to the second, divide (integer) the sum by the third number and
+# multiply the result by the fourth number.
+# Print the result.
+# Input	    Output
+# 10        30
+# 20
+# 3
+# 3
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+forth_num = int(input())
+
+print(f"{int((first_num + second_num) / third_num) * forth_num:.0f}")
+
 
