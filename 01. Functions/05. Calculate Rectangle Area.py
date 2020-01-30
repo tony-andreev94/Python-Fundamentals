3a04d1b665c05cc88e6b98450358d728cf5ba063
tony-andreev94 <tony.andreev94@outlook.com>
2020-01-30 22:21:36 +0200

    # Basic function tasks added.

---
+++ 01. Functions/05. Calculate Rectangle Area.py
@@ -0,0 +1,17 @@
+# Create a function that calculates and returns the area of a rectangle by given width and height:
+#
+# Input	    Output
+# 3         12
+# 4
+#
+# 6         12
+# 2
+
+
+def rectangle_func(a, b):
+    return a * b
+
+side_a = int(input())
+side_b = int(input())
+
+print(rectangle_func(side_a, side_b))
